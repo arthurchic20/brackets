c3bc3027f1a3869ab011cf7a7029360280bfb28d
arthurchic20 <arthurchic20@gmail.com>
2023-02-08 14:45:49 +0600

    init: start and end project brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let strArr = str.split('')
+	let count = true
+	do {
+		nextPrime:
+		for (let i = 0 ; i < strArr.length; i++) { 
+			for (let x = 0; x < bracketsConfig.length; x++){
+				let el = bracketsConfig[x]
+				if (strArr[i] === el[0] && strArr[i + 1] === el[1]) {
+					strArr.splice(i, 2)
+					count = true
+					break nextPrime
+				} else {
+					count = false
+					continue
+				}
+			}
+		}
+		if (strArr.length == 0) count = false
+	} while (count)	
+	return strArr.length != 0 ? false : true 
 }
